072dd73a9780d8608e0c8663aef11508bc90ceea
Alesha314 <55951687+T0PT@users.noreply.github.com>
2023-02-04 18:06:20 +0100

    Add files via upload

--- Neuro_paint.py
+++ Neuro_paint.py
@@ -7,56 +7,67 @@ wind= Tk()
 wind.title('PAINt')
 wind.config(bg='white')
 cn=Canvas(width=781,height=781,bg='white')
-field= [[0]*28]*28#[[0 for i in range(28)] for i in range(28)]
-lastfield=[[0]*28]*28# [vertical y][horizontal x], 28x28 from left to right, top to bottom   
-field_to_feed=[[0]*28]*28
+field= [[0 for i in range(28)] for i in range(28)] # [vertical y][horizontal x], 28x28 from left to right, top to bottom 
+#lastfield=[[0 for i in range(28)] for i in range(28)]  
+field_to_feed=[0]*784
 
 for i in range(27):
     cn.create_line(i*28+27,0,i*28+27,839)
     cn.create_line(0,i*28+27,839,i*28+27)
 
-def update_cn(event=0):
+def update_cn(event=0, new=False):
     global field
-    global lastfield
-    for y in range(0,28):
-        for x in range(0,28):
-            if field[y][x]!=lastfield[y][x]:
-                col=field[y][x]
-                cn.create_rectangle(x*28,y*28,x*28+26,y*28+26, fill=f'#{255-col:02x}{255-col:02x}{255-col:02x}', outline=f'#{255-col:02x}{255-col:02x}{255-col:02x}')                            
-    print('update')
-    lastfield=field
+    #global lastfield
+    global field_to_feed
+    if new==True:
+        cn.delete("all")
+        for i in range(27):
+            cn.create_line(i*28+27,0,i*28+27,839)
+            cn.create_line(0,i*28+27,839,i*28+27)
+    else:
+        for y in range(0,28):
+            for x in range(0,28):
+                if field[y][x]!=0:
+                    col=field[y][x]
+                    cn.create_rectangle(x*28,y*28,x*28+26,y*28+26, fill=f'#{255-col:02x}{255-col:02x}{255-col:02x}', outline=f'#{255-col:02x}{255-col:02x}{255-col:02x}') 
+                    field_to_feed[y*28+x]=col                           
+                #print('update')
+    #lastfield=field.copy()
+    
 
 def clear():     
     global field 
-    field=[[0]*28]*28       
+    field=[[0 for i in range(28)] for i in range(28)]
+    update_cn(new=True)     
 
 def draw(event):    
     global field
     if event.x<784 and event.y<784:
         cn.create_rectangle(event.x//28*28,event.y//28*28,event.x//28*28+26,event.y//28*28+26, fill='black')
         #print('x'+str(event.x//28)+' y'+str(event.y//28))
-        field[0][0]=30 # from left to right and up to down, y is vertical, x is horizontal        
-        # try:
-        #     field[event.y//28-1][event.x//28]+=15
-        #     if field[event.y//28-1][event.x//28]>255: field[event.y//28-1][event.x//28]=255
-        # except:
-        #     pass
-        # try:
-        #     field[event.y//28][event.x//28+1]+=15 
-        #     if field[event.y//28][event.x//28+1]>255: field[event.y//28][event.x//28+1]=255           
-        # except:
-        #     pass
-        # try:
-        #     field[event.y//28+1][event.x//28]+=15     
-        #     if field[event.y//28+1][event.x//28]>255: field[event.y//28+1][event.x//28]=255      
-        # except:
-        #     pass
-        # try:
-        #     field[event.y//28][event.x//28-1]+=15 
-        #     if field[event.y//28][event.x//28-1]>255: field[event.y//28][event.x//28-1]=255          
-        # except:
-        #     pass    
-    for i in range(28):print(field[i])       
+        field[event.y//28][event.x//28]=+180 # from left to right and up to down, y is vertical, x is horizontal        
+        try:
+            field[event.y//28-1][event.x//28]+=20
+            if field[event.y//28-1][event.x//28]>255: field[event.y//28-1][event.x//28]=255
+        except:
+            pass
+        try:
+            field[event.y//28][event.x//28+1]+=20
+            if field[event.y//28][event.x//28+1]>255: field[event.y//28][event.x//28+1]=255           
+        except:
+            pass
+        try:
+            field[event.y//28+1][event.x//28]+=20   
+            if field[event.y//28+1][event.x//28]>255: field[event.y//28+1][event.x//28]=255      
+        except:
+            pass
+        try:
+            field[event.y//28][event.x//28-1]+=20
+            if field[event.y//28][event.x//28-1]>255: field[event.y//28][event.x//28-1]=255          
+        except:
+            pass    
+        print('draw')
+    #for i in range(28):print(field[i])       
 
 def undraw(event):
     if event.x<838 and event.y<838:
